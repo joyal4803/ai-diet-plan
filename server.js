975a0e9a2ea69e471afb2b040e0ff5f925b24208
Joyal Thomas <71383512+joyal4803@users.noreply.github.com>
2025-07-06 19:58:51 +0530

    server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // server.js
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
-require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
